35d8dfa12bd59cab764531b3092fe6b000965397
nick <kolangavrish0001@ukr.net>
2023-01-05 11:56:48 +0000

    Home & Team Chnages

--- src/views/TeamPage.vue
+++ src/views/TeamPage.vue
@@ -54,8 +54,8 @@
                 </v-card>
             </v-col>
         </v-row>
-        <v-row align="center" justify="center" class="marr">
-            <v-col md="6" sm="6" class="mt-5">
+        <v-row>
+            <v-col md="6" sm="6" class="mt-5 right-p-mar">
                 <v-card class="mx-auto pa-12 pb-5 rounded" 
                         min-height="" 
                         max-width="600px" 
@@ -77,6 +77,28 @@
                     <v-card-subtitle class="pl-0 pt-2 text-f" style="font-size: 18px">Co-Founder / CXO </v-card-subtitle>
                 </v-card>
             </v-col>
+            <v-col md="6" sm="6" class="mt-5 left-p-mar">
+                <v-card class="mx-auto pa-12 pb-5 rounded" 
+                        min-height="" 
+                        max-width="600px" 
+                        outlined 
+                        style="background:#B9D9EB"
+                >
+                    <v-img :src="aescobar_pfp"/>
+                    <v-card-title style="font-size: 24px;" class="pl-0 text-f">Aescobar
+                        <v-btn
+                            class="mx-3"
+                            small
+                            dark
+                            href="https://twitter.com/aescobarindo" target="_blank"
+                            color="transparent"
+                        >
+                            <v-icon color="red" >$vuetify.icons.twitter</v-icon>
+                        </v-btn>
+                    </v-card-title> 
+                    <v-card-subtitle class="pl-0 pt-2 text-f" style="font-size: 18px">Community Manager</v-card-subtitle>
+                </v-card>
+            </v-col>
         </v-row>
     </v-container>
 </template>
@@ -84,11 +106,13 @@
 import huge_pfp from "../assets/TeamPage/HugeBoba_pfp.png"
 import akz_pfp from "../assets/TeamPage/Akz_pfp.png"
 import pro_pfp from "../assets/TeamPage/pro_pfp.png"
+import aescobar_pfp from "../assets/TeamPage/aescobar_pfp.png"
 export default {
     data: () => ({
         huge_pfp: huge_pfp,
         akz_pfp: akz_pfp, 
         pro_pfp: pro_pfp,
+        aescobar_pfp: aescobar_pfp,
     }),
 }
 </script>
